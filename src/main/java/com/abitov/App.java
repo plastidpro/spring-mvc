7e63510d55cf12ab1cb360a9b7f77573f5e2a71c
plastidpro <abitovpro@gmail.com>
2017-12-19 21:53:37 +0600

    second commit

--- src/main/java/com/abitov/App.java
+++ src/main/java/com/abitov/App.java
@@ -1,14 +1,14 @@
 package com.abitov;
 
-import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
-public class App implements WebApplicationInitializer {
+public class App extends AbstractAnnotationConfigDispatcherServletInitializer {
 
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
@@ -18,5 +18,20 @@ public class App implements WebApplicationInitializer {
         ServletRegistration.Dynamic registration = servletContext.addServlet("dispatcherServlet", new DispatcherServlet(webContext));
         registration.setLoadOnStartup(1);
         registration.addMapping("/");
+    }
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
     }
 }
--- src/main/java/com/abitov/WebConfig.java
+++ src/main/java/com/abitov/WebConfig.java
@@ -3,14 +3,22 @@ package com.abitov;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.abitov.controllers")
-public class WebConfig  {
+public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Bean
     public ViewResolver viewResolver(){
@@ -22,5 +30,22 @@ public class WebConfig  {
         return resolver;
     }
 
+
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry){
+        super.addViewControllers(registry);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry){
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("/images/")
+                .setCacheControl(CacheControl.maxAge(10L, TimeUnit.MILLISECONDS).cachePublic());
+
+        registry.addResourceHandler("/favicon.ico")
+                .addResourceLocations("resources/favicon.ico");
+
+    }
 
 }
